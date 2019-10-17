d434433b66596a05816ca78a726a86db3a605868
u04 <chowkas@rogers.com>
2019-10-17 18:46:16 -0400

    first commit

---
+++ Assets/_Scripts/PlayerAnimState.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Util
+{
+    [System.Serializable]
+    public enum PlayerAnimState
+    {
+        IDLE,
+        RUN,
+        JUMP
+    }
+}
+
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Util;
+
+public class PlayerController : MonoBehaviour
+{
+    public PlayerAnimState playerAnimState;
+    public Animator playerAnimator;
+    public SpriteRenderer playerSpriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnimState = PlayerAnimState.IDLE;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //idle state
+        if (Input.GetAxis("Horizontal") == 0)
+        {
+            playerAnimState = PlayerAnimState.IDLE;
+            playerAnimator.SetInteger("AnimState", (int) PlayerAnimState.IDLE);
+        }
+        //move right
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            playerAnimState = PlayerAnimState.RUN;
+            playerAnimator.SetInteger("AnimState", (int) PlayerAnimState.RUN);
+            playerSpriteRenderer.flipX = false;
+        }
+        //move left
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            playerAnimState = PlayerAnimState.RUN;
+            playerAnimator.SetInteger("AnimState", (int) PlayerAnimState.RUN);
+            playerSpriteRenderer.flipX = true;
+        }
+        //jump
+        if (Input.GetAxis("Jump") > 0)
+        {
+            playerAnimState = PlayerAnimState.JUMP;
+            playerAnimator.SetInteger("AnimState", (int) PlayerAnimState.JUMP);
+        }
+
+    }
+}
