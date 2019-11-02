5b361e7d4923ee7ed38b840e8fb687cc71afcd60
cdey1 <300920267@cencol.ca>
2019-11-02 14:41:26 -0400

    player can move

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -8,7 +8,9 @@ public class PlayerController : MonoBehaviour
     public PlayerAnimState playerAnimState;
     public Animator playerAnimator;
     public SpriteRenderer playerSpriteRenderer;
-    // Start is called before the first frame update
+    public Rigidbody2D playerRigidbody2D;
+    public float moveForce;
+    public float jumpForce;
     void Start()
     {
         playerAnimState = PlayerAnimState.IDLE;
@@ -29,6 +31,7 @@ public class PlayerController : MonoBehaviour
             playerAnimState = PlayerAnimState.RUN;
             playerAnimator.SetInteger("AnimState", (int) PlayerAnimState.RUN);
             playerSpriteRenderer.flipX = false;
+            playerRigidbody2D.AddForce(Vector2.right * moveForce);
         }
         //move left
         if (Input.GetAxis("Horizontal") < 0)
@@ -36,12 +39,14 @@ public class PlayerController : MonoBehaviour
             playerAnimState = PlayerAnimState.RUN;
             playerAnimator.SetInteger("AnimState", (int) PlayerAnimState.RUN);
             playerSpriteRenderer.flipX = true;
+            playerRigidbody2D.AddForce(Vector2.left * moveForce);
         }
         //jump
         if (Input.GetAxis("Jump") > 0)
         {
             playerAnimState = PlayerAnimState.JUMP;
             playerAnimator.SetInteger("AnimState", (int) PlayerAnimState.JUMP);
+            playerRigidbody2D.AddForce(Vector2.up * jumpForce);
         }
 
     }
