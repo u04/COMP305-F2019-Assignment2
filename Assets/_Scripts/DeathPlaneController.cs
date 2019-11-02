f8a375d2b238b33d7a45284b302f436e64065745
cdey1 <300920267@cencol.ca>
2019-11-02 17:16:37 -0400

    m

---
+++ Assets/_Scripts/DeathPlaneController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DeathPlaneController : MonoBehaviour
+{
+    public GameObject activeCheckpoint;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(other)
+            //watched up to 49:34 le5 part 2 
+    }
+}
